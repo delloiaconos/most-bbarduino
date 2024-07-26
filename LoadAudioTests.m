79558fdb5ea1dfaf574bce368fb19e4247c24416
Salvatore Dello Iacono <delloiaconos@gmail.com>
2024-07-26 17:30:09 +0200

    Added sound play.

--- LoadAudioTests.m
+++ LoadAudioTests.m
@@ -4,17 +4,24 @@ baseDir = "dataset/acquisizione01/";
 fName = fullfile( baseDir, "008_008.RAW" );
 
 fr = fopen( fName, "rb" );
-data = fread( fr, 'int16' );
+signal = fread( fr, 'int16' );
 fclose( fr );
 
 Fs = 16000; % 16 kHz
 
-t_all = (0:length( data )-1)*(1/Fs);
+t_all = (0:length( signal )-1)*(1/Fs);
+
+
+len = length( signal );
+sx = signal( 1:2:len );
 
 fig = figure();
-    plot( t_all, data );
+    plot( t_all, signal );
 
 
-[f, Y]  = myfft( data, Fs );
+[f, Y]  = myfft( sx, Fs );
 fig = figure();
     plot( f, Y );
+
+player = audioplayer( sx, Fs );
+play( player )
